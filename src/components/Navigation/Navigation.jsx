4f48d87cd3f777d9063d23c29429d8c30586c7a4
Artem <artmimiron@gmail.com>
2020-11-18 21:26:46 +0200

    /

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -5,13 +5,13 @@ export default function Navigation() {
     return (
         <ul className='Navigation'>
               <li className='Navigation-Link'>
-                <NavLink to="/" className='Navigation-Text'>
+                <NavLink to="/New_user" className='Navigation-Text'>
                   <img className='Navigation-Img' src="./add_user.png" alt=""/>
                   <span>ADD</span>
                 </NavLink>
               </li>
               <li className='Navigation-Link' >
-                <NavLink to="/UserList" className='Navigation-Text'>
+                <NavLink to="/User_list" className='Navigation-Text'>
                   <img className='Navigation-Img' src="./user_list.png" alt=""/>
                   <span>LIST</span>
                 </NavLink>
